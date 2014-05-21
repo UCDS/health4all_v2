f12c77485e95b5e7299eefd310e1e2109716ff89
ajith555 <ajith.shetty07@gmail.com>
2014-05-21 11:23:12 +0530

    Second commit -controller with master table removed

---
+++ application/views/templates/diagnosis_leftnav.php
@@ -0,0 +1,14 @@
+<div class="col-sm-3 col-md-2 sidebar-left">
+    <ul class="nav nav-sidebar">
+
+		<li class="nav-header">Add</li>
+
+		<li><a href="<?php echo base_url();?>diagnostics/add/test_method">Test Method</a></li>
+		<li><a href="<?php echo base_url();?>diagnostics/add/test_group">Test Group</a></li>
+		
+		<li class="nav-header">Edit</li>
+		<li><a href="<?php echo base_url();?>diagnostics/edit/test_method">Test Method</a></li>
+		<li><a href="<?php echo base_url();?>diagnostics/edit/test_group">Test Group</a></li>
+		
+</ul>
+</div>			
