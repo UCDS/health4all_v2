f27000e1c75e950468090828d25bb1b68fda9cea
root <root@localhost.localdomain>
2014-08-25 14:02:17 +0530

    Database changes

--- application/config/database.php
+++ application/config/database.php
@@ -51,7 +51,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'password';
 $db['default']['database'] = 'health4all';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
