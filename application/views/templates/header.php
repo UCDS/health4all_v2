f83293a65d38006aceabc2a05939ef21a556e11b
Vivek <vivek@yousee.in>
2015-03-16 18:08:43 +0530

    Font icons added

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<title><?php echo $title; ?> - Health4All</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css" media='screen,print'>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css" >
 	
 	<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
